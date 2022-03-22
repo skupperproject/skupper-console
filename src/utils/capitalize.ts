2a5ef79e22484f43f5254847e83208e42b399a5b
valerio bartolini <vabarjs@gmail.com>
2022-03-22 09:44:41 +0100

    chore: adding eslint ts-doc

--- src/utils/capitalize.ts
+++ src/utils/capitalize.ts
@@ -1,7 +1,13 @@
-export function capitalizeFirstLetter(string: string) {
+/**
+ * Get a text and return it Capitalized.
+ */
+export function capitalizeFirstLetter(string: string): string {
   return string.trim().replace(/^\w/, (c) => c.toUpperCase());
 }
 
-export function capitalizeAllFirstLetters(string: string) {
+/**
+ * Get a text and return it Capitalized for all the words.
+ */
+export function capitalizeAllFirstLetters(string: string): string {
   return string.replace(/\w\S*/g, (w) => w.replace(/^\w/, (c) => c.toUpperCase()));
 }
