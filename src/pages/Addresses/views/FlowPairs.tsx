9bae240d609b7ef24af7cafd1e8a2d36f9be8d8e
valerio bartolini <vabarjs@gmail.com>
2022-10-27 16:48:30 +0200

    feat: add charts to address detail page

--- src/pages/Addresses/views/FlowPairs.tsx
+++ src/pages/Addresses/views/FlowPairs.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 
+import { ChartPie } from '@patternfly/react-charts';
 import {
     Breadcrumb,
     BreadcrumbHeading,
     BreadcrumbItem,
     Card,
+    CardBody,
+    CardTitle,
     Flex,
+    Grid,
+    GridItem,
     Pagination,
-    Stack,
-    StackItem,
     Tab,
     Tabs,
     TabTitleText,
@@ -19,21 +22,24 @@ import {
 import { useQuery } from '@tanstack/react-query';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 
+import EmptyData from '@core/components/EmptyData';
+import RealTimeLineChart from '@core/components/RealTimeLineChart';
+import { ChartThemeColors } from '@core/components/RealTimeLineChart/RealTimeLineChart.enum';
 import ResourceIcon from '@core/components/ResourceIcon';
+import { formatByteRate, formatBytes } from '@core/utils/formatBytes';
 import { ErrorRoutesPaths, HttpStatusErrors } from '@pages/shared/Errors/errors.constants';
 import LoadingPage from '@pages/shared/Loading';
 import { UPDATE_INTERVAL } from 'config';
 
 import { CONNECTIONS_PAGINATION_SIZE_DEFAULT } from '../Addresses.constants';
-import {
-    FlowPairDetailsLabels,
-    AddressesRoutesPathLabel,
-    AddressesRoutesPaths,
-} from '../Addresses.enum';
+import { FlowPairsLabels, AddressesRoutesPathLabel, AddressesRoutesPaths } from '../Addresses.enum';
 import FlowsPairsTable from '../components/FlowPairsTable';
 import AddressProcessesTable from '../components/ProcessesTable';
 import { AddressesController } from '../services';
 import { QueriesAddresses } from '../services/services.enum';
+
+const REAL_TIME_CONNECTION_HEIGHT_CHART = 350;
+const ITEM_DISPLAY_COUNT = 6;
 
 const FlowsPairs = function () {
     const navigate = useNavigate();
@@ -110,9 +116,30 @@ const FlowsPairs = function () {
 
     const { connections, total } = connectionsPaginated;
 
+    const topClientsMap = connections.reduce((acc, { processId, processName, bytes }, index) => {
+        if (index < ITEM_DISPLAY_COUNT) {
+            acc[processName] = { name: processName, value: (acc[processId]?.value || 0) + bytes };
+        }
+
+        return acc;
+    }, {} as Record<string, { name: string; value: number }>);
+    const topClients = Object.values(topClientsMap);
+
+    const totalBytesSent = connections.reduce((acc, { bytes }) => acc + bytes, 0);
+    const totalBytesReceived = connections.reduce((acc, { targetBytes }) => acc + targetBytes, 0);
+
+    const AvgByteRateSent = Math.round(
+        connections.reduce((acc, { byteRate }) => acc + byteRate, 0) / connections.length || 1,
+    );
+
+    const AvgByteRateReceived = Math.round(
+        connections.reduce((acc, { targetByteRate }) => acc + targetByteRate, 0) /
+            connections.length || 1,
+    );
+
     return (
-        <Stack hasGutter>
-            <StackItem>
+        <Grid hasGutter>
+            <GridItem>
                 <Breadcrumb>
                     <BreadcrumbItem>
                         <Link to={AddressesRoutesPaths.Addresses}>
@@ -121,28 +148,122 @@ const FlowsPairs = function () {
                     </BreadcrumbItem>
                     <BreadcrumbHeading to="#">{addressName}</BreadcrumbHeading>
                 </Breadcrumb>
-            </StackItem>
+            </GridItem>
 
-            <StackItem>
+            <GridItem span={8} rowSpan={2}>
+                <Card style={{ height: `${REAL_TIME_CONNECTION_HEIGHT_CHART}px` }}>
+                    <CardTitle>{FlowPairsLabels.Connections}</CardTitle>
+                    {connections?.length ? (
+                        <RealTimeLineChart
+                            options={{
+                                height: REAL_TIME_CONNECTION_HEIGHT_CHART,
+                                padding: {
+                                    top: 0,
+                                    bottom: 50,
+                                    left: 50,
+                                    right: 20,
+                                },
+                            }}
+                            data={[{ name: 'Connections', value: connections.length }]}
+                        />
+                    ) : (
+                        <EmptyData />
+                    )}
+                </Card>
+            </GridItem>
+            <GridItem span={2}>
+                <Card isFullHeight>
+                    <CardTitle>Total Bytes TX</CardTitle>
+                    <CardBody>{formatBytes(totalBytesSent)}</CardBody>
+                </Card>
+            </GridItem>
+            <GridItem span={2}>
+                <Card isFullHeight>
+                    <CardTitle>Total Bytes RX</CardTitle>
+                    <CardBody>{formatBytes(totalBytesReceived)}</CardBody>
+                </Card>
+            </GridItem>
+            <GridItem span={2}>
+                <Card isFullHeight>
+                    <CardTitle>Average Byte rate TX</CardTitle>
+                    <CardBody>{formatByteRate(AvgByteRateSent)}</CardBody>
+                </Card>
+            </GridItem>
+            <GridItem span={2}>
+                <Card isFullHeight>
+                    <CardTitle>Average Byte rate RX</CardTitle>
+                    <CardBody>{formatByteRate(AvgByteRateReceived)}</CardBody>
+                </Card>
+            </GridItem>
+            <GridItem span={6}>
+                <Card style={{ height: `${REAL_TIME_CONNECTION_HEIGHT_CHART}px` }}>
+                    <CardTitle>{FlowPairsLabels.FlowPairsDistributionTitle}</CardTitle>
+                    <ChartPie
+                        constrainToVisibleArea
+                        data={topClients?.map(({ name, value }) => ({
+                            x: name,
+                            y: formatBytes(value),
+                        }))}
+                        labels={topClients?.map(
+                            ({ name, value }) => `${name}: ${formatBytes(value)}`,
+                        )}
+                        legendData={topClients?.map(({ name }) => ({ name }))}
+                        legendOrientation="vertical"
+                        legendPosition="right"
+                        padding={{
+                            bottom: 100,
+                            left: -100,
+                            right: 100,
+                            top: 0,
+                        }}
+                        themeColor={ChartThemeColors.Multi}
+                        height={REAL_TIME_CONNECTION_HEIGHT_CHART}
+                    />
+                </Card>
+            </GridItem>
+            <GridItem span={6}>
+                <Card style={{ height: `${REAL_TIME_CONNECTION_HEIGHT_CHART}px` }}>
+                    <CardTitle>{FlowPairsLabels.FlowPairsDistributionTitle}</CardTitle>
+                    {topClients?.length ? (
+                        <RealTimeLineChart
+                            options={{
+                                type: 'area',
+                                formatter: formatBytes,
+                                height: REAL_TIME_CONNECTION_HEIGHT_CHART,
+                                padding: {
+                                    top: 0,
+                                    bottom: 130,
+                                    left: 70,
+                                    right: 20,
+                                },
+                            }}
+                            data={topClients}
+                        />
+                    ) : (
+                        <EmptyData />
+                    )}
+                </Card>
+            </GridItem>
+            <GridItem>
                 <Flex alignItems={{ default: 'alignItemsCenter' }}>
                     <ResourceIcon type="address" />
                     <TextContent>
                         <Text component={TextVariants.h1}>{addressName}</Text>
                     </TextContent>
                 </Flex>
-            </StackItem>
-            <StackItem isFilled>
+            </GridItem>
+            <GridItem>
                 <Card isRounded className="pf-u-pt-md">
                     <Tabs activeKey={vanAddressView} onSelect={handleTabClick}>
                         <Tab
                             eventKey={0}
-                            title={<TabTitleText>{FlowPairDetailsLabels.Servers}</TabTitleText>}
+                            title={<TabTitleText>{FlowPairsLabels.Servers}</TabTitleText>}
                         >
                             <AddressProcessesTable processes={processes} />
                         </Tab>
                         <Tab
                             eventKey={1}
-                            title={<TabTitleText>{FlowPairDetailsLabels.Connections}</TabTitleText>}
+                            title={<TabTitleText>{FlowPairsLabels.Connections}</TabTitleText>}
                         >
                             <FlowsPairsTable flowPairs={connections} />
                             {!!connections.length && (
@@ -159,8 +280,8 @@ const FlowsPairs = function () {
                         </Tab>
                     </Tabs>
                 </Card>
-            </StackItem>
-        </Stack>
+            </GridItem>
+        </Grid>
     );
 };
 
