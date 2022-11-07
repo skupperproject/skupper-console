c2605367effb3a37a6f42316101971aae816099d
valerio bartolini <vabarjs@gmail.com>
2022-11-07 18:09:29 +0100

    chore: activate integration test on github

--- src/pages/Addresses/Addresses.constants.ts
+++ src/pages/Addresses/Addresses.constants.ts
@@ -22,14 +22,3 @@ export const FlowPairsColumns = [
 ];
 
 export const CONNECTIONS_PAGINATION_SIZE_DEFAULT = 20;
-
-// PROCESSES TABLE
-export const ProcessesColumns = [
-    { name: FlowPairsColumnsNames.Site, prop: 'siteName' },
-    { name: FlowPairsColumnsNames.Process, prop: 'processName' },
-    { name: FlowPairsColumnsNames.Host, prop: 'host' },
-    { name: FlowPairsColumnsNames.Port, prop: 'port' },
-    { name: FlowPairsColumnsNames.ImageName, prop: 'imageName' },
-    { name: FlowPairsColumnsNames.Bytes, prop: 'bytes' },
-    { name: FlowPairsColumnsNames.ByteRate, prop: 'byteRate' },
-];
