570aeb7d8bd8c53f4fabb81b9e49ac0511790490
valerio bartolini <vabarjs@gmail.com>
2022-11-29 23:48:02 +0100

    refactor: hide vanflow

--- src/core/components/Graph/Graph.ts
+++ src/core/components/Graph/Graph.ts
@@ -102,15 +102,6 @@ export default class Graph {
             .attr('viewBox', `0 0 ${this.width} ${this.height}`);
     }
 
-    private fixNodes = (x: number, y: number) => {
-        this.nodes.forEach(function (node) {
-            if (x !== node.x || y !== node.y) {
-                node.fx = node.x;
-                node.fy = node.y;
-            }
-        });
-    };
-
     private dragStarted = ({ active }: { active: boolean }, node: GraphNode) => {
         if (!active) {
             this.force.alphaTarget(0.3).restart();
@@ -119,7 +110,6 @@ export default class Graph {
         node.fx = node.x;
         node.fy = node.y;
 
-        this.fixNodes(node.x, node.y);
         this.isDraggingNode = true;
     };
 
@@ -146,13 +136,7 @@ export default class Graph {
             this.force.alphaTarget(0.3).restart();
         }
 
-        this.force.nodes().forEach((node) => {
-            node.fx = node.x;
-            node.fy = node.y;
-        });
-
-        this.force
-            .nodes()
+        this.nodes
             .filter(({ group }) => group === Number(groupId))
             .forEach((node) => {
                 node.groupFx = x;
@@ -163,8 +147,7 @@ export default class Graph {
     };
 
     private groupDragged = ({ x, y }: { x: number; y: number }, groupId: string) => {
-        this.force
-            .nodes()
+        this.nodes
             .filter(({ group }) => group === Number(groupId))
             .forEach((node) => {
                 node.fx = node.x + x - (node.groupFx || 0);
@@ -336,6 +319,11 @@ export default class Graph {
                     this.EventEmitter.emit(GraphEvents.IsGraphLoaded, [this.nodes]);
                     this.isGraphLoaded = true;
                 }
+
+                this.nodes.forEach((node) => {
+                    node.fx = node.x;
+                    node.fy = node.y;
+                });
 
                 this.force.stop();
             });
