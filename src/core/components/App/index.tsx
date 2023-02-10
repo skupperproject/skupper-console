e25a3bf1857a768ebca9f2c7608e09689bc4b7ee
valerio bartolini <vabarjs@gmail.com>
2023-02-10 14:53:29 +0100

    feat: add redirection when app start from root

--- src/core/components/App/index.tsx
+++ src/core/components/App/index.tsx
@@ -1,17 +1,29 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 
 import { Page } from '@patternfly/react-core';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import AppContent from '@layout/AppContent';
 import Header from '@layout/Header';
 import SideBar from '@layout/SideBar';
+import { REDIRECT_TO_PATH } from 'config';
 import { routes } from 'routes';
+
 import '@patternfly/patternfly/patternfly.css';
 import '@patternfly/patternfly/patternfly-addons.css';
 
 import './App.css';
 
 const App = function () {
+    const navigate = useNavigate();
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        if (pathname === '/') {
+            navigate(REDIRECT_TO_PATH);
+        }
+    }, [pathname, navigate]);
+
     return (
         <Page
             header={<Header />}
@@ -19,7 +31,7 @@ const App = function () {
             isManagedSidebar
             className="app-main-container"
         >
-            <Suspense fallback={<div />}>
+            <Suspense fallback={<span />}>
                 <AppContent>{routes}</AppContent>
             </Suspense>
         </Page>
