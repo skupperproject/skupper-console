967e939ffbc7d3c2463dcc041c239da857e3c914
valerio bartolini <vabarjs@gmail.com>
2023-04-06 19:55:26 +0200

    feat(Core): :sparkles: Add error boundary

--- src/pages/Addresses/services/index.ts
+++ src/pages/Addresses/services/index.ts
@@ -9,22 +9,26 @@ export const AddressesController = {
       tcpActiveFlows
     }: { httpTotalFlows: PrometheusApiResultSingle[]; tcpActiveFlows: PrometheusApiResultSingle[] }
   ) => {
-    const tcpActiveFlowsMap = tcpActiveFlows.reduce((acc, flow) => {
-      acc[flow.metric.address] = Number(flow.value[1]) / 2;
+    const tcpActiveFlowsMap =
+      tcpActiveFlows.length &&
+      tcpActiveFlows.reduce((acc, flow) => {
+        acc[flow.metric.address] = Number(flow.value[1]) / 2;
 
-      return acc;
-    }, {} as Record<string, number>);
+        return acc;
+      }, {} as Record<string, number>);
 
-    const httpTotalFlowsMap = httpTotalFlows?.reduce((acc, flow) => {
-      acc[flow.metric.address] = Number(flow.value[1]) / 2;
+    const httpTotalFlowsMap =
+      httpTotalFlows.length &&
+      httpTotalFlows?.reduce((acc, flow) => {
+        acc[flow.metric.address] = Number(flow.value[1]) / 2;
 
-      return acc;
-    }, {} as Record<string, number>);
+        return acc;
+      }, {} as Record<string, number>);
 
     return addresses.map((address) => ({
       ...address,
-      totalFlows: Math.round(httpTotalFlowsMap[address.name]),
-      currentFlows: Math.round(tcpActiveFlowsMap[address.name])
+      totalFlows: httpTotalFlowsMap ? Math.round(httpTotalFlowsMap[address.name]) : 0,
+      currentFlows: tcpActiveFlowsMap ? Math.round(tcpActiveFlowsMap[address.name]) : 0
     }));
   }
 };
