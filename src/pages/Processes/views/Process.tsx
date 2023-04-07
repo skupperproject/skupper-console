ed18dc7ebc9bd9ac16fa82c06e2180ed71b6ccd7
valerio bartolini <vabarjs@gmail.com>
2023-04-07 18:14:50 +0200

    chore: :hammer: Improve tsconfig for eslint

--- src/pages/Processes/views/Process.tsx
+++ src/pages/Processes/views/Process.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { Grid, GridItem } from '@patternfly/react-core';
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
