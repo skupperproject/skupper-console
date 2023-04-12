cfa604c2bdcf376161503d64dfa67d7f9f873934
valerio bartolini <vabarjs@gmail.com>
2023-04-12 19:33:59 +0200

    fix(Process): :lady_beetle: Change metrics from iRate to Rate
    BREAKING CHANGE: ]

--- src/API/Prometheus.queries.ts
+++ src/API/Prometheus.queries.ts
@@ -9,8 +9,10 @@ export const timeIntervalMap: IntervalTimeMap = {
   oneHours: { value: '1h', seconds: 3600, step: '15s', key: 'oneHours', label: '1 hour' },
   twoHours: { value: '2h', seconds: 2 * 3600, step: '30s', key: 'twoHours', label: '2 hours' },
   sixHours: { value: '6h', seconds: 6 * 3600, step: '1m', key: 'sixHours', label: '6 hours' },
-  twelveHours: { value: '12h', seconds: 12 * 3600, step: '5m', key: 'twelveHours', label: '12 hours' },
-  oneDay: { value: '1d', seconds: 24 * 3600, step: '5m', key: 'oneDay', label: '1 day' },
+  twelveHours: { value: '12h', seconds: 12 * 3600, step: '2m', key: 'twelveHours', label: '12 hours' },
+  oneDay: { value: '1d', seconds: 24 * 3600, step: '4m', key: 'oneDay', label: '1 day' },
+  twoDay: { value: '2d', seconds: 2 * 24 * 3600, step: '9m', key: 'twoDay', label: '2 day' },
+  threeDay: { value: '3d', seconds: 3 * 24 * 3600, step: '12m', key: 'threeDay', label: '3 day' },
   oneWeek: { value: '1w', seconds: 7 * 24 * 3600, step: '10m', key: 'oneWeek', label: '1 week' },
   twoWeeks: { value: '2w', seconds: 14 * 24 * 3600, step: '20m', key: 'twoWeeks', label: '2 weeks' }
 };
@@ -37,21 +39,21 @@ export const queries = {
     return `sum(http_requests_method_total{${param}})`;
   },
   getTotalRequestRateByMethod(param: string, range: IntervalTimePropValue) {
-    return `sum by (method)(irate(http_requests_method_total{${param}}[${range}]))`;
+    return `sum by (method)(rate(http_requests_method_total{${param}}[${range}]))`;
   },
   getHttpPartialStatus(param: string) {
     return `sum by (partial_code) (label_replace(http_requests_result_total{${param}},"partial_code", "$1", "code","(.*).{2}"))`;
   },
   getHttpPartialStatusRate(param: string, range: IntervalTimePropValue) {
-    return `sum by (partial_code) (label_replace(irate((http_requests_result_total{${param}}[${range}])),"partial_code", "$1", "code","(.*).{2}"))`;
+    return `sum by (partial_code) (label_replace(rate((http_requests_result_total{${param}}[${range}])),"partial_code", "$1", "code","(.*).{2}"))`;
   },
 
   // latency queries
   getQuantile(param: string, range: IntervalTimePropValue, quantile: 0.5 | 0.9 | 0.99) {
-    return `histogram_quantile(${quantile},sum(irate(flow_latency_microseconds_bucket{${param}}[${range}]))by(le))`;
+    return `histogram_quantile(${quantile},sum(rate(flow_latency_microseconds_bucket{${param}}[${range}]))by(le))`;
   },
   getAvgLatency(param: string, range: IntervalTimePropValue) {
-    return `sum(irate(flow_latency_microseconds_sum{${param}}[${range}])/irate(flow_latency_microseconds_count{${param}}[${range}]))`;
+    return `sum(rate(flow_latency_microseconds_sum{${param}}[${range}])/rate(flow_latency_microseconds_count{${param}}[${range}]))`;
   },
 
   // data transfer queries
@@ -59,7 +61,7 @@ export const queries = {
     return `sum by(direction)(octets_total{${paramSource}} or octets_total{${paramDest}})`;
   },
   getByteRateByDirection(paramSource: string, paramDest: string, range: IntervalTimePropValue) {
-    return `sum by(direction)(irate(octets_total{${paramSource}}[${range}]) or irate(octets_total{${paramDest}}[${range}]))`;
+    return `sum by(direction)(rate(octets_total{${paramSource}}[${range}]) or rate(octets_total{${paramDest}}[${range}]))`;
   },
 
   // counters for addresses
