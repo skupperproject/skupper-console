6e6179ab77d48b9ec793e0d1b058c1a38d13cb4e
valerio bartolini <vabarjs@gmail.com>
2023-05-19 12:56:58 +0200

    feat(Topology): :sparkles: Change protocol labels color based on the protocol

--- src/config/testIds.config.ts
+++ src/config/testIds.config.ts
@@ -6,7 +6,6 @@ export const getTestsIds = {
   siteView: (id: string) => `sk-site-view-${id}`,
   componentView: (id: string) => `sk-component-view-${id}`,
   processView: (id: string) => `sk-process-view-${id}`,
-
   navbarComponent: () => 'sk-nav-bar-component',
   breadcrumbComponent: () => 'sk-breadcrumb'
 };
