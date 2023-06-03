e263f4baeb865339fea7f7e50a2b58d3d51f9593
valerio bartolini <vabarjs@gmail.com>
2023-06-03 13:04:15 +0200

    fix(General): :lady_beetle: Details button doesn't show links

--- src/core/components/ViewDetailsCell/index.tsx
+++ src/core/components/ViewDetailsCell/index.tsx
@@ -2,15 +2,24 @@ import { useCallback } from 'react';
 
 import { Button } from '@patternfly/react-core';
 import { SearchIcon } from '@patternfly/react-icons';
+import { Link } from 'react-router-dom';
 
 import { ViewDetailCellProps } from './ViewDetailCell.interfaces';
 
-const ViewDetailCell = function ({ value, onClick }: ViewDetailCellProps) {
+const ViewDetailCell = function ({ link, value, onClick }: ViewDetailCellProps) {
   const handleOnClick = useCallback(() => {
     if (onClick) {
       onClick(value);
     }
   }, [value, onClick]);
+
+  if (link) {
+    return (
+      <Link to={link}>
+        <SearchIcon />
+      </Link>
+    );
+  }
 
   return <Button variant="link" onClick={handleOnClick} icon={<SearchIcon />} />;
 };
