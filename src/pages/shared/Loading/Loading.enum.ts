362597d0cb2a5d8059df98a7915990c43d85ca7b
Valerio <vabarjs@gmail.com>
2023-06-20 11:34:45 +0200

    refactor(General): :recycle: Remove skupper references to title and messages

--- src/pages/shared/Loading/Loading.enum.ts
+++ src/pages/shared/Loading/Loading.enum.ts
@@ -2,5 +2,5 @@ export enum Labels {
   LoadingTitle = 'Fetching data',
   LoadingMessage = 'The data for the service network is being retrieved. One moment please...',
   LoadingBrandTitle = 'Connect',
-  LoadingBrandMessage = 'A Skupper network management and visualization tool.'
+  LoadingBrandMessage = 'A network management and visualization tool.'
 }
