ffc4edde486c72adb542877aa18746ce9ceb2bb4
Valerio <vabarjs@gmail.com>
2023-08-21 16:19:04 +0200

    refactor(Topology): :recycle: Group prometheus queries for processes

--- src/pages/Topology/Topology.interfaces.ts
+++ src/pages/Topology/Topology.interfaces.ts
@@ -1,9 +1,23 @@
 import { ModelStyle } from '@antv/g6';
 
+import { PrometheusApiSingleResult } from '@API/Prometheus.interfaces';
+
 export interface Entity {
   id: string;
   comboId?: string;
   label: string;
   img: string;
   nodeConfig?: ModelStyle;
+}
+
+export interface TopologyMetrics {
+  bytesByProcessPairs: PrometheusApiSingleResult[];
+  byteRateByProcessPairs: PrometheusApiSingleResult[];
+  latencyByProcessPairs: PrometheusApiSingleResult[];
+}
+
+export interface TopologyMetricsMetrics {
+  showBytes?: boolean;
+  showByteRate?: boolean;
+  showLatency?: boolean;
 }
