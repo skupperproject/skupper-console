69c010d5ae242508bf811f1844c1a7e624450efb
Valerio <vabarjs@gmail.com>
2023-08-28 18:28:52 +0200

    refactor(Services): :recycle: Clean up sankey

--- src/pages/Addresses/services/index.ts
+++ src/pages/Addresses/services/index.ts
@@ -1,3 +1,4 @@
+import { DEFAULT_SANKEY_CHART_FLOW_VALUE } from '@core/components/SKSanckeyChart';
 import { PrometheusApiSingleResult } from 'API/Prometheus.interfaces';
 import { AddressResponse } from 'API/REST.interfaces';
 import { VarColors } from 'colors';
@@ -58,26 +59,29 @@ export const AddressesController = {
     }));
   },
 
-  getNodesAndLinksResources: (servicePairs: PrometheusApiSingleResult[], isMetricEnabled = false) => {
-    const separator = '@_@';
-    const sourceNodes =
+  convertToSankeyChartData: (servicePairs: PrometheusApiSingleResult[], withMetric = false) => {
+    const separator = '@_@'; // This is an internal team role to unify asiteId and a siteName in prometheus
+    const sourceProcessSuffix = 'client'; // The Sankey chart crashes when the same site is present in both the client and server positions. No circular dependency are allowed for this kind of chart
+
+    const clients =
       servicePairs?.map(({ metric }) => ({
-        id: `${metric.sourceProcess || metric.sourceSite?.split(separator)[0]} (client)`,
+        id: `${metric.sourceProcess || metric.sourceSite?.split(separator)[0]} ${sourceProcessSuffix}`,
         nodeColor: metric.sourceProcess ? VarColors.Blue400 : undefined
       })) || [];
-    const destNodes =
+
+    const servers =
       servicePairs?.map(({ metric }) => ({
         id: metric.destProcess || metric.destSite?.split(separator)[0],
         nodeColor: metric.destProcess ? VarColors.Blue400 : undefined
       })) || [];
 
-    const nodes = [...sourceNodes, ...destNodes].filter((v, i, a) => a.findIndex((v2) => v2.id === v.id) === i);
+    const nodes = [...clients, ...servers].filter((v, i, a) => a.findIndex((v2) => v2.id === v.id) === i);
 
     const links =
       servicePairs?.map(({ metric, value }) => ({
-        source: `${metric.sourceProcess || metric.sourceSite?.split(separator)[0]} (client)`,
+        source: `${metric.sourceProcess || metric.sourceSite?.split(separator)[0]} ${sourceProcessSuffix}`,
         target: metric.destProcess || metric.destSite?.split(separator)[0],
-        value: isMetricEnabled ? Number(value[1]) : 0.01
+        value: withMetric ? Number(value[1]) : DEFAULT_SANKEY_CHART_FLOW_VALUE // The Nivo sankey chart restricts the usage of the value 0 for maintaining the height of each flow. We use a value near 0
       })) || [];
 
     return { nodes, links };
