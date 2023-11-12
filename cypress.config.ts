6338946cf16b1d3a42d0bbced2112e8a8de3d677
bartoval <vabarjs@gmail.com>
2023-11-12 16:48:45 +0100

    chore(General): :hammer: Remove webpack preprocessor

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,19 +1,9 @@
 import { defineConfig } from 'cypress';
 
-const webpack = require('@cypress/webpack-preprocessor');
-
-const options = {
-  webpackOptions: require('./webpack.dev.js'),
-  watchOptions: {}
-};
-
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     video: false,
-    screenshotOnRunFailure: false,
-    setupNodeEvents(on) {
-      on('file:preprocessor', webpack(options));
-    }
+    screenshotOnRunFailure: false
   }
 });
