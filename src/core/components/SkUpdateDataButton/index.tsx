8d6f1bca4220075f6de1b542c6975a01cfb58b36
Paul Wright <pwright@redhat.com>
2024-02-18 15:45:01 +0000

    avoid jargon with 'refetch'

--- src/core/components/SkUpdateDataButton/index.tsx
+++ src/core/components/SkUpdateDataButton/index.tsx
@@ -21,7 +21,7 @@ interface SkUpdateDataButtonProps {
   refreshIntervalDefault?: number;
 }
 
-const REFETCH_DATA_LABEL = 'Refetch';
+const REFETCH_DATA_LABEL = 'Fetch';
 export const refreshDataIntervalMap = [
   {
     key: 'pause',
