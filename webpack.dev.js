44b411823c72e7b45f244b234f88e02c47c7bfc9
bartoval <vabarjs@gmail.com>
2024-05-09 13:10:49 +0200

    chore(General): :hammer: Remove circular dependencies lib

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,6 @@
 const { merge } = require('webpack-merge');
 
-const CircularDependencyPlugin = require('circular-dependency-plugin');
-
 const commonConfig = require('./webpack.common');
-
-const MAX_CYCLES = 5;
-let numCyclesDetected = 0;
 
 const devConfig = {
   mode: 'development',
@@ -15,30 +10,6 @@ const devConfig = {
     historyApiFallback: true,
     compress: true
   },
-
-  plugins: [
-    new CircularDependencyPlugin({
-      exclude: /node_modules/,
-      failOnError: true,
-      allowAsyncCycles: false,
-      cwd: process.cwd(),
-      onStart({ _ }) {
-        numCyclesDetected = 0;
-        numCyclesDisplayed = 0;
-      },
-      onDetected({ module: _, paths, compilation }) {
-        numCyclesDetected++;
-        compilation.warnings.push(new Error(paths.join(' -> ')));
-      },
-      onEnd({ compilation }) {
-        if (numCyclesDetected > MAX_CYCLES) {
-          compilation.errors.push(
-            new Error(`Detected ${numCyclesDetected} cycles which exceeds configured limit of ${MAX_CYCLES}`)
-          );
-        }
-      }
-    })
-  ],
 
   module: {
     rules: [
