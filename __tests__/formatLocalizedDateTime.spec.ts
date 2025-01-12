6ed98b73ab94387a079a962258ec01017a61db58
bartoval <vabarjs@gmail.com>
2025-01-12 19:23:45 +0100

    test: :white_check_mark: Remove global jest references

--- __tests__/formatLocalizedDateTime.spec.ts
+++ __tests__/formatLocalizedDateTime.spec.ts
@@ -1,5 +1,3 @@
-import { expect } from '@jest/globals';
-
 import { formatLocalizedDateTime } from '../src/core/utils/formatLocalizedDateTime';
 
 describe('formatLocalizedDateTime', () => {
--- __tests__/getValueFromNestedProperty.spec.ts
+++ __tests__/getValueFromNestedProperty.spec.ts
@@ -1,5 +1,3 @@
-import { expect } from '@jest/globals';
-
 import { getValueFromNestedProperty } from '../src/core/utils/getValueFromNestedProperty';
 
 type TestObject = {
